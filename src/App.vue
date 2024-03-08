0e10285baca2532ada81f5ddb75b425883b9b36a
xloc <xloc.cc@outlook.com>
2024-03-07 23:46:28 -0800

    add pdf viewer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
 
+import { VuePDF, usePDF } from '@tato30/vue-pdf'
+import '@tato30/vue-pdf/style.css'
+
+const pdfURL = 'https://arxiv.org/pdf/1506.02640.pdf'
+const { pdf, pages } = usePDF(pdfURL)
+
 </script>
 
 <template>
-  Hello World
+<div class="flex w-screen h-screen items-stretch">
+  <div class="flex-1 overflow-y-auto">
+    <div v-for="page in pages" :key="page">
+      <VuePDF :pdf="pdf" :page="page" text-layer annotation-layer fit-parent />
+    </div>
+  </div>
+  <hr class="border-r border-r-slate-400 h-full">
+  <div class="flex-1">
+    asd
+  </div>
+</div>
 </template>
